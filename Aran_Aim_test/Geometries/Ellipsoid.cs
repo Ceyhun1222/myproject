61e5043e3e0d60f90c14b4c5049b7d84102d2b1a
jeyhunhasanov <jeyhunhasanov@risk.az>
2019-05-21 13:28:20 +0000

    Added 4 files to Geometries

---
+++ Aran_Aim_test/Geometries/Ellipsoid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Aran.Package;
+
+namespace Aran.Geometries.SpatialReferences
+{
+	public class Ellipsoid : IPackable
+	{
+		public SpatialReferenceGeoType SrGeoType{get;set;}
+        public double SemiMajorAxis { get; set; }
+        public double Flattening { get; set; }
+        public bool IsValid { get; set; }
+
+		public Ellipsoid()
+		{
+			SrGeoType = SpatialReferenceGeoType.srgtWGS1984;
+			SemiMajorAxis = 0;
+			Flattening = 0;
+			IsValid = false;
+		}
+
+
+        public void Pack (PackageWriter writer)
+        {
+            writer.PutEnum<SpatialReferenceGeoType> (SrGeoType);
+            writer.PutDouble (SemiMajorAxis);
+            writer.PutDouble (Flattening);
+            writer.PutBool (IsValid);
+        }
+
+        public void Unpack (PackageReader reader)
+        {
+            SrGeoType = reader.GetEnum<SpatialReferenceGeoType> ();
+            SemiMajorAxis = reader.GetDouble ();
+            Flattening = reader.GetDouble ();
+            IsValid = reader.GetBool ();
+        }
+    }
+}
